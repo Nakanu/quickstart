a026f54f3267d3ac1b43c999e1fb4597dfccd828
haizhoumo <haizhoumo@google.com>
2019-11-12 11:25:06 -0800

    4. Services

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -2,20 +2,31 @@ import 'package:angular/angular.dart';
 import 'package:angular_app/src/hero.dart';
 import 'src/mock_heroes.dart';
 import 'src/hero_component.dart';
+import 'src/hero_service.dart';
 
 @Component(
   selector: 'my-app',
   templateUrl: 'app_component.html',
   directives: [coreDirectives, HeroComponent],
   styleUrls: ['app_component.css'],
+  providers: [ClassProvider(HeroService)],
 )
-class AppComponent {
+class AppComponent implements OnInit {
   final title = 'Tour of Heroes';
   Hero selected;
-  List<Hero> heroes = mockHeroes;
+  List<Hero> heroes;
+  final HeroService _heroService;
+
+  AppComponent(this._heroService);
 
   void onSelect(Hero hero) {
     if (selected == hero) selected = null;
     else selected = hero;
   }
+
+  Future<void> _getHeroes() async {
+    heroes = await _heroService.getAll();
+  }
+
+  void ngOnInit() => this._getHeroes();
 }
